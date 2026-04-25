e5cf4f22cb18e145254aacd0900c9d9cc1ef3a0a
JohnKreye <jokr6224@colorado.edu>
2026-04-25 16:00:40 -0600

    RingGameBoard construction fix

--- src/main/java/GameBoard/RingGameBoard.java
+++ src/main/java/GameBoard/RingGameBoard.java
@@ -14,7 +14,6 @@ public class RingGameBoard extends GameBoard {
         //A ring has a hole in it; we will make that hole into invalid points
         int minDimension = Math.min(boardHeight, boardWidth);
         int holeOffset = minDimension/5;
-
         if (
             (point.x > holeOffset && point.x < boardWidth - holeOffset) &&
             (point.y > holeOffset && point.y < boardHeight - holeOffset)
@@ -32,15 +31,14 @@ public class RingGameBoard extends GameBoard {
         for(int row = 0; row < boardHeight; row++) {
             int column = 0;
             for(; column < boardWidth - 5; column += 5) {
-                computePointValidness(new Point(row, column + 0));
-                computePointValidness(new Point(row, column + 1));
-                computePointValidness(new Point(row, column + 2));
-                computePointValidness(new Point(row, column + 3));
-                computePointValidness(new Point(row, column + 4));
+                computePointValidness(new Point(column + 0, row));
+                computePointValidness(new Point(column + 1, row));
+                computePointValidness(new Point(column + 2, row));
+                computePointValidness(new Point(column + 3, row));
+                computePointValidness(new Point(column + 4, row));
             }
-            column -= 5;
             for(; column < boardWidth; column++) {
-                computePointValidness(new Point(row, column));
+                computePointValidness(new Point(column, row));
             }
         }
     }
