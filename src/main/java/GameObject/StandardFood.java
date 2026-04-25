b99a9a0e3e265af0d84376729901849df77fd775
JohnKreye <jokr6224@colorado.edu>
2026-04-25 16:00:20 -0600

    StandardFood now allows for custom growth amount

--- src/main/java/GameObject/StandardFood.java
+++ src/main/java/GameObject/StandardFood.java
@@ -6,20 +6,26 @@ import Snake.Snake;
 import java.awt.*;
 
 public class StandardFood extends ConsumeableObject{
+    private int growthAmount;
 
     public StandardFood(Point objectPosition) {
+        this(objectPosition, 1);
+    }
+
+    public StandardFood(Point objectPosition, int growthAmount) {
         Color objectColor = Color.red;
+        this.growthAmount = growthAmount;
         super(objectColor, objectPosition);
     }
 
     @Override
     public void interact(Game game) {
-        game.gameLogic.addScore(10);
         super.interact(game);
+        game.gameLogic.addScore(10);
     }
 
     @Override
     public void applyEffectTo(Snake snake) {
-        snake.grow(1);
+        snake.grow(growthAmount);
     }
 }
